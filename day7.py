7969883f8e9ae6721a1e3ae4b9933575318f324c
Simon Gao <simon.gao@workday.com>
2020-12-06 21:48:41 -0800

    minor day 7 clean up

--- day7.py
+++ day7.py
@@ -10,45 +10,38 @@ bags: Dict[str, Bag] = {}
 
 data = []
 with open('day7input') as f:
-  data = [s.strip('.\n') for s in f.readlines()]
+  data = [s.strip('.\n').replace(' bags', '').replace(' bag', '') for s in f.readlines()]
 
 def build_graph():
   for d in data:
     name, ref = d.split(' contain ')
-    name = name[:-5]
-    if not name in bags:
+    if name not in bags:
       bags[name] = Bag()
     if not ref.startswith('no'):
       for bag in ref.split(', '):
         k, v = bag.split(' ', 1)
-        v = v[:-4].strip()
         bags[name].contains[v] = int(k)
-        if not v in bags:
+        if v not in bags:
           bags[v] = Bag()
         bags[v].containedBy.add(name)
+
+build_graph()
 
 def print_graph():
   for k, v in bags.items():
     print(f'{k}: {v.contains} & {v.containedBy}')
 
-build_graph()
-
 def find_contained_by(name: str, visited: Set[str]) -> int:
   count = 0
   for bag in bags[name].containedBy:
-    if not bag in visited:
-      count += 1
+    if bag not in visited:
       visited.add(bag)
-      count += find_contained_by(bag, visited)
+      count += 1 + find_contained_by(bag, visited)
   return count
 
 print(find_contained_by('shiny gold', set())) # part 1
 
 def find_contains(name: str) -> int:
-  count = 0
-  for k, v in bags[name].contains.items():
-    count += v
-    count += v * find_contains(k)
-  return count
+  return sum(v + v * find_contains(k) for k, v in bags[name].contains.items())
 
 print(find_contains('shiny gold')) # part 2
