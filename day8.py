10eccf9f45c785d98c11a7f8d8ce445debdd6f80
Simon Gao <simon.gao@workday.com>
2020-12-07 21:39:37 -0800

    looks better now

--- day8.py
+++ day8.py
@@ -1,15 +1,13 @@
-import functools
-import re
-from typing import Callable, Dict, List, Set, Tuple
+from typing import List, Set, Tuple
 
-def execute(instructions: List[Tuple[str, int]]) -> int:
+def execute(instructions: List[Tuple[str, int]], fix_mode: bool) -> int:
   accumulator = 0
   executed_lines: Set[int] = set()
   current = 0
   while current < len(instructions):
     ins = instructions[current]
     if current in executed_lines:
-      return -1
+      return -1 if fix_mode else accumulator
     else:
       executed_lines.add(current)
     if ins[0] == 'acc':
@@ -20,23 +18,22 @@ def execute(instructions: List[Tuple[str, int]]) -> int:
     current += 1
   return accumulator
 
-def deep_copy(data: List[Tuple[str, int]]) -> List[Tuple[str, int]]:
-  return [x[:] for x in data]
+def swap(instructions: List[Tuple[str, int]], line: int) -> List[Tuple[str, int]]:
+  copy = [x[:] for x in data] # deep copy
+  copy[line] = ('nop' if copy[line][0] == 'jmp' else 'jmp', copy[line][1])
+  return copy
+
+def fix_program(instructions: List[Tuple[str, int]]) -> int:
+  for i in range(len(instructions)):
+    if instructions[i][0] == 'jmp' or instructions[i][0] == 'nop':
+      copy = swap(instructions, i)
+      if (result := execute(copy, True)) > -1:
+        return result
+  return result
 
 if __name__ == "__main__":
   data: List[Tuple[str, int]] = []
   with open('day8input') as f:
-    tmp = [s.strip('\n').split(' ', 1) for s in f.readlines()]
-    data = [(s[0], int(s[1])) for s in tmp]
-  print(execute(data)) # part 1
-  for i in range(len(data)):
-    if data[i][0] == 'jmp':
-      copy = deep_copy(data)
-      copy[i] = ('nop', copy[i][1])
-      if (result := execute(copy)) > -1:
-        print(result)
-    if data[i][0] == 'nop':
-      copy = deep_copy(data)
-      copy[i] = ('jmp', copy[i][1])
-      if (result := execute(copy)) > -1:
-        print(result)
+    data = [(lambda x: (x[0], int(x[1])))(s.strip('\n').split(' ', 1)) for s in f.readlines()]
+  print(execute(data, False)) # part 1
+  print(fix_program(data))
