ba72a94b3990d2736a094938a8ba39f97b50dfcf
Simon Gao <simon.gao@workday.com>
2020-12-10 22:47:29 -0800

    looking better now

--- day11.py
+++ day11.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from itertools import product
 import math
 from operator import add, mul
 from typing import List
@@ -7,31 +7,22 @@ def print_processed(data: List[str]):
   for d in data:
     print(d)
 
+def move(i: int) -> int:
+  return i if i == 0 else int(i+i/abs(i))
+
 def is_occupied(data: List[str], i, j, idir, jdir, first) -> bool:
-  if first:
-    if i+idir >= 0 and i+idir <= len(data)-1 and j+jdir >= 0 and j+jdir <= len(data[0])-1:
-      return data[i+idir][j+jdir] == '#'
-  else:
-    while i+idir >= 0 and i+idir <= len(data)-1 and j+jdir >= 0 and j+jdir <= len(data[0])-1:
-      return data[i+idir][j+jdir] == '#' if data[i+idir][j+jdir] != '.' else is_occupied(data, i, j, idir-1 if idir < 0 else idir+1 if idir > 0 else idir, jdir-1 if jdir < 0 else jdir+1 if jdir > 0 else jdir, first)
+  while i+idir >= 0 and i+idir <= len(data)-1 and j+jdir >= 0 and j+jdir <= len(data[0])-1:
+    return data[i+idir][j+jdir] == '#' if first or data[i+idir][j+jdir] != '.' else is_occupied(data, i, j, move(idir), move(jdir), first)
   return False
 
 def process(data: List[str], occupiedlimit: int, first: bool) -> List[str]:
-  processed: List[str] = data[:]
+  processed: List[str] = [''] * len(data)
   for i in range(len(data)):
     tmp = list(data[i])
     for j in range(len(data[0])):
       if data[i][j] == '.':
         continue
-      occupiedcount = 0
-      occupiedcount += is_occupied(data, i, j, -1, 0, first)
-      occupiedcount += is_occupied(data, i, j, -1, -1, first)
-      occupiedcount += is_occupied(data, i, j, -1, +1, first)
-      occupiedcount += is_occupied(data, i, j, 0, -1, first)
-      occupiedcount += is_occupied(data, i, j, 0, +1, first)
-      occupiedcount += is_occupied(data, i, j, +1, 0, first)
-      occupiedcount += is_occupied(data, i, j, +1, -1, first)
-      occupiedcount += is_occupied(data, i, j, +1, +1, first)
+      occupiedcount = sum(is_occupied(data, i, j, idir, jdir, first) for idir, jdir in product([-1, 0, 1], [-1, 0, 1]) if idir != 0 or jdir != 0)
       if occupiedcount == 0 and data[i][j] == 'L':
         tmp[j] = '#'
       elif data[i][j] == '#' and occupiedcount >= occupiedlimit:
@@ -44,8 +35,7 @@ def process(data: List[str], occupiedlimit: int, first: bool) -> List[str]:
 def stablize(data: List[str], occupiedlimit: int, first: bool) -> int:
   next = process(data, occupiedlimit, first)
   while next != data:
-    data = next
-    next = process(data, occupiedlimit, first)
+    data, next = next, process(next, occupiedlimit, first)
   return sum(s.count('#') for s in data)
 
 if __name__ == "__main__":
