12d2e7c4cb82563bede720360aecb6eada466dfb
Simon Gao <simon.gao@workday.com>
2020-12-17 23:10:04 -0800

    should not have spent the time to refactor after finishing part 1

--- day18.py
+++ day18.py
@@ -11,31 +11,56 @@ def evaluate(formula: List[str]) -> int:
   paren: List[int] = []
   op: List[str] = []
   for i in formula:
-    print(f'{result} , {paren}, {op}')
     if i == ' ':
       continue
-    elif i == '+' or i == '*':
+    print(f'{result} , {paren}, {op}')
+    if i == '+':
       op.append(i)
-    elif i == '(':
+    elif i == '(' or i == '*':
       paren.append(result)
-      op.append('+')
+      op.append(i)
       result = 0
     elif i.isnumeric():
-      operator = '+' if len(op) == 0 else op.pop()
-      if operator == '+':
+      if result == 0:
+        result = int(i)
+      elif op.pop() == '+':
         result += int(i)
       else:
         result *= int(i)
     else:
-      if len(op) > 0:
-        if op.pop() == '+':
+      closed = False
+      while len(op) > 0:
+        if closed and op[-1] == '(':
+          break
+        o = op.pop()
+        if o == '(':
+          closed = True
+          if len(op) > 0 and op[-1] != '(':
+            continue
+          else:
+            if paren[-1] == 0:
+              paren.pop()
+            break
+        while len(paren) > 0 and paren[-1] == 0:
+          paren.pop()
+          continue
+        if o == '+':
           result += int(paren.pop())
         else:
-          result *= int(paren.pop())
+          if closed:
+            op.append(o)
+            break
+          else:
+            result *= int(paren.pop())
+  while len(op) > 0:
+    op.pop()
+    result *= paren.pop()
   return result
 
 if __name__ == "__main__":
   data = []
   with open('day18input') as f:
     data = [list(s.strip('\n')) for s in f.readlines()]
+  for a in data:
+    print(f'{"".join(a)}: {evaluate(a)}')
   print(sum(evaluate(f) for f in data))
